86bb23739ddab14067e931c6cf49e61f7a40cbf5
Bruno Luvizotto <luvizotto@bnl.gov>
2018-01-17 11:36:39 -0500

    Add analog pizzabox to profile_collection

--- startup/10-detectors.py
+++ startup/10-detectors.py
@@ -345,3 +345,235 @@ class PizzaBoxFS(Device):
 
 pb1 = PizzaBoxFS('XF:07BM-CT{Enc01', name = 'pb1')
 pb1.enc1.pulses_per_deg = 9400000/360
+
+
+class Adc(Device):
+    file_size = Cpt(EpicsSignal, '}FileSize')
+    reset = Cpt(EpicsSignal, '}Rst-Cmd')
+    filepath = Cpt(EpicsSignal, '}ID:File.VAL', string=True)
+    sec_array = Cpt(EpicsSignal, '}T:sec_Bin_')
+    nsec_array = Cpt(EpicsSignal, '}T:nsec_Bin_')
+    #pos_array = Cpt(EpicsSignal, '}Cnt:Pos_Bin_')
+    index_array = Cpt(EpicsSignal, '}Cnt:Index_Bin_')
+    data_array = Cpt(EpicsSignal, '}Data_Bin_')
+    sample_rate = Cpt(EpicsSignal,'}F:Sample-I_', write_pv='}F:Sample-SP')
+    enable_averaging = Cpt(EpicsSignal, '}Avrg-Sts', write_pv='}Avrg-Sel')
+    averaging_points = Cpt(EpicsSignal, '}Avrg-SP')
+    averaging_points_rbv = Cpt(EpicsSignal, '}GP-ADC:Reg0-RB_')
+    volt_array = Cpt(EpicsSignal, '}V-I')
+    volt = Cpt(EpicsSignal, '}E-I')
+    offset = Cpt(EpicsSignal, '}Offset')
+    dev_name = Cpt(EpicsSignal, '}DevName')
+    dev_saturation = Cpt(EpicsSignal, '}DevSat')
+    polarity = 'neg'
+
+    enable_sel = Cpt(EpicsSignal, '}Ena-Sel')
+    enable_rb = Cpt(EpicsSignal, '}Ena-Sts')
+
+    def timeout_handler(self, signum, frame):
+        print("{}.connected timeout".format(self.name))
+        raise Exception("end of time")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._ready_to_collect = False
+
+        #signal.signal(signal.SIGALRM, self.timeout_handler)
+        #signal.setitimer(signal.ITIMER_REAL, 2)
+        #try:
+        #    while(self.connected == False):
+        #        pass
+        if self.connected:
+            #self.enable_sel.put(1)
+            #self.sample_rate.put(350)
+            self.enable_averaging.put(1)
+            if self.averaging_points.value == 0:
+                self.averaging_points.put("1024")
+        #except Exception as exc:
+        #    pass
+        #signal.alarm(0)
+
+
+
+class AdcFS(Adc):
+    "Adc Device, when read, returns references to data in filestore."
+    chunk_size = 1024
+
+    def __init__(self, *args, reg, **kwargs):
+        self._reg = reg
+        super().__init__(*args, **kwargs)
+
+    def stage(self):
+        "Set the filename and record it in a 'resource' document in the filestore database."
+
+
+        if(self.connected):
+            print(self.name, 'stage')
+            DIRECTORY = '/GPFS/xf08id/'
+            rpath = 'pizza_box_data'
+            filename = 'an_' + str(uuid.uuid4())[:6]
+            full_path = os.path.join(rpath, filename)
+            self._full_path = os.path.join(DIRECTORY, full_path)  # stash for future reference
+            if len(self._full_path) > 40:
+                raise RuntimeError("Stupidly, EPICS limits the file path to 80 characters. "
+                               "Choose a different DIRECTORY with a shorter path. (I know....)")
+            self._full_path = os.path.join(DIRECTORY, full_path)  # stash for future reference
+            self.filepath.put(self._full_path)
+            self.resource_uid = self._reg.register_resource(
+                'PIZZABOX_AN_FILE_TXT',
+                DIRECTORY, full_path,
+                {'chunk_size': self.chunk_size})
+
+            super().stage()
+
+    def unstage(self):
+        if(self.connected):
+            set_and_wait(self.enable_sel, 1)
+            return super().unstage()
+
+    def kickoff(self):
+        print('kickoff', self.name)
+        self._ready_to_collect = True
+        "Start writing data into the file."
+
+        set_and_wait(self.enable_sel, 0)
+
+        # Return a 'status object' that immediately reports we are 'done' ---
+        # ready to collect at any time.
+        return NullStatus()
+
+    def complete(self):
+        print('complete', self.name, '| filepath', self._full_path)
+        if not self._ready_to_collect:
+            raise RuntimeError("must called kickoff() method before calling complete()")
+        # Stop adding new data to the file.
+        set_and_wait(self.enable_sel, 1)
+        return NullStatus()
+
+    def collect(self):
+        """
+        Record a 'datum' document in the filestore database for each encoder.
+
+        Return a dictionary with references to these documents.
+        """
+        print('collect', self.name)
+        self._ready_to_collect = False
+
+        # Create an Event document and a datum record in filestore for each line
+        # in the text file.
+        now = ttime.time()
+        ttime.sleep(1)  # wait for file to be written by pizza box
+        if os.path.isfile(self._full_path):
+            with open(self._full_path, 'r') as f:
+                linecount = 0
+                for ln in f:
+                    linecount += 1
+
+            chunk_count = linecount // self.chunk_size + int(linecount % self.chunk_size != 0)
+            for chunk_num in range(chunk_count):
+                datum_uid = self._reg.register_datum(self.resource_uid,
+                                                     {'chunk_num': chunk_num})
+                data = {self.name: datum_uid}
+
+                yield {'data': data,
+                       'timestamps': {key: now for key in data}, 'time': now}
+        else:
+            print('collect {}: File was not created'.format(self.name))
+
+    def describe_collect(self):
+        # TODO Return correct shape (array dims)
+        now = ttime.time()
+        return {self.name: {self.name:
+                     {'filename': self._full_path,
+                      'devname': self.dev_name.value,
+                      'source': 'pizzabox-adc-file',
+                      'external': 'FILESTORE:',
+                      'shape': [5,],
+                      'dtype': 'array'}}}
+
+
+class PizzaBoxAnalogFS(Device):
+    #internal_ts_sel = Cpt(EpicsSignal, 'Gen}T:Internal-Sel')
+
+    adc1 = Cpt(AdcFS, 'ADC:1', reg=db.reg)
+    adc6 = Cpt(AdcFS, 'ADC:6', reg=db.reg)
+    adc7 = Cpt(AdcFS, 'ADC:7', reg=db.reg)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # must use internal timestamps or no bytes are written
+        # self.stage_sigs[self.internal_ts_sel] = 1
+
+    def kickoff(self):
+        "Call encoder.kickoff() for every encoder."
+        for attr_name in ['adc1']: #, 'adc2', 'adc3', 'adc4']:
+            status = getattr(self, attr_name).kickoff()
+        # it's fine to just return one of the status objects
+        return status
+
+    def collect(self):
+        "Call adc.collect() for every encoder."
+        # Stop writing data to the file in all encoders.
+        for attr_name in ['adc1']: #, 'adc2', 'adc3', 'adc4']:
+            set_and_wait(getattr(self, attr_name).enable_sel, 0)
+        # Now collect the data accumulated by all encoders.
+        for attr_name in ['adc1']: #, 'adc2', 'adc3', 'adc4']:
+            yield from getattr(self, attr_name).collect()
+
+
+pba1 = PizzaBoxAnalogFS('XF:07BMB-CT{GP1-', name = 'pba1')
+
+
+class PizzaBoxEncHandlerTxt(HandlerBase):
+    encoder_row = namedtuple('encoder_row',
+                             ['ts_s', 'ts_ns', 'encoder', 'index', 'state'])
+    "Read PizzaBox text files using info from filestore."
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+        cs = self.chunk_size
+        return [self.encoder_row(*(int(v) for v in ln.split()))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+
+class PizzaBoxDIHandlerTxt(HandlerBase):
+    di_row = namedtuple('di_row', ['ts_s', 'ts_ns', 'encoder', 'index', 'di'])
+    "Read PizzaBox text files using info from filestore."
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+        cs = self.chunk_size
+        return [self.di_row(*(int(v) for v in ln.split()))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+
+class PizzaBoxAnHandlerTxt(HandlerBase):
+    encoder_row = namedtuple('encoder_row', ['ts_s', 'ts_ns', 'index', 'adc'])
+    "Read PizzaBox text files using info from filestore."
+
+    bases = (10, 10, 10, 16)
+    def __init__(self, fpath, chunk_size):
+        self.chunk_size = chunk_size
+        with open(fpath, 'r') as f:
+            self.lines = list(f)
+
+    def __call__(self, chunk_num):
+
+        cs = self.chunk_size
+        return [self.encoder_row(*(int(v, base=b) for v, b in zip(ln.split(), self.bases)))
+                for ln in self.lines[chunk_num*cs:(chunk_num+1)*cs]]
+
+
+
+db.reg.register_handler('PIZZABOX_AN_FILE_TXT',
+                        PizzaBoxAnHandlerTxt, overwrite=True)
+db.reg.register_handler('PIZZABOX_ENC_FILE_TXT',
+                        PizzaBoxEncHandlerTxt, overwrite=True)
+db.reg.register_handler('PIZZABOX_DI_FILE_TXT',
+                        PizzaBoxDIHandlerTxt, overwrite=True)
