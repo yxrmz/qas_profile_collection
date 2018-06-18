34a0f82af3ace6734daa189b54dab56cb668ea2d
C De Silva <desilva@bnl.gov>
2018-06-18 12:24:19 -0400

    Added aliases for amps

--- startup/85-aliases.py
+++ startup/85-aliases.py
@@ -1,13 +1,18 @@
 #these aliases are used to connect pizza boxes and amplifiers
 
 
-i0 = pba1.adc6
+i0 = pba1.adc3
 i0_amp.par = i0
 i0.amp = i0_amp
 
-it = pba1.adc7
+it = pba1.adc4
 it_amp.par = it
 it.amp = it_amp
+
+ir = pba1.adc5
+ir_amp.par = ir
+ir.amp = ir_amp
+
 '''
 iff = pba1.adc6
 iff_amp.par = iff
