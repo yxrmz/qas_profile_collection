a7479ed55c21af73eb09d2d6a21a37e554a8e616
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-11-15 14:24:58 -0500

    ENH: added logging to a log file for msg_hook

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,7 +1,9 @@
 print(__file__)
 import nslsii
 
-nslsii.configure_base(get_ipython().user_ns, 'qas', bec=False)
+beamline_id = 'qas'
+
+nslsii.configure_base(get_ipython().user_ns, beamline_id, bec=False)
 
 
 # At the end of every run, verify that files were saved and
@@ -12,8 +14,8 @@ from bluesky.callbacks.broker import verify_files_saved
 # Optional: set any metadata that rarely changes.
 
 # these should *always* be QAS
-RE.md['group'] = 'qas'
-RE.md['beamline_id'] = 'QAS'
+RE.md['group'] = beamline_id
+RE.md['beamline_id'] = beamline_id.upper()
 # isstools reads these
 
 # check these keys exist, if not set to default
@@ -42,3 +44,34 @@ from pyOlog.ophyd_tools import *
 # import logging
 # ophyd.logger.setLevel(logging.DEBUG)
 # logging.basicConfig(level=logging.DEBUG)
+
+# Enabling (2018C3) logging of the message hook into a log file.
+import os
+import sys
+import datetime
+import functools
+
+# TODO: to be removed and imported from bluesky once
+# https://github.com/NSLS-II/bluesky/pull/1117 is reviewed, merged, released,
+# and pushed to the beamline.
+def ts_msg_hook(msg, file=sys.stdout):
+    t = '{:%H:%M:%S.%f}'.format(datetime.datetime.now())
+    msg_fmt = '{: <17s} -> {!s: <15s} args: {}, kwargs: {}'.format(
+        msg.command,
+        msg.obj.name if hasattr(msg.obj, 'name') else msg.obj,
+        msg.args,
+        msg.kwargs)
+    print('{} {}'.format(t, msg_fmt), file=file)
+
+# The logs will be saved to the profile dir.
+profile_startup_dir = get_ipython().profile_dir.startup_dir
+# The name of the log file consists of the beamline id and the timestamp at the
+# startup of bsui, so we don't have collisions of the names.
+log_filename = f'{beamline_id}-bsui-{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.log'
+log_filename = os.path.join(profile_startup_dir, log_filename)
+
+print(f'\n!!! The logs will be written to {file_name} !!!\n')
+file = open(log_filename, 'a')
+
+func = functools.partial(ts_msg_hook, file=file)
+RE.msg_hook = func
