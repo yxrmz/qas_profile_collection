892fe5b131cee6b0780733454a35c779b3eeaf85
Maksim Rakitin <maxim.rakitin@gmail.com>
2018-11-15 14:27:20 -0500

    FIX: correct the log filename

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -70,7 +70,7 @@ profile_startup_dir = get_ipython().profile_dir.startup_dir
 log_filename = f'{beamline_id}-bsui-{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.log'
 log_filename = os.path.join(profile_startup_dir, log_filename)
 
-print(f'\n!!! The logs will be written to {file_name} !!!\n')
+print(f'\n!!! The logs will be written to {log_filename} !!!\n')
 file = open(log_filename, 'a')
 
 func = functools.partial(ts_msg_hook, file=file)
