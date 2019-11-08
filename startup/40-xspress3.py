4ccb430170c21df31b3a8acef9660732f701dd36
Maksim Rakitin <mrakitin@bnl.gov>
2019-11-08 17:54:27 -0500

    XS flyer (WIP)

--- startup/40-xspress3.py
+++ startup/40-xspress3.py
@@ -54,12 +54,18 @@ class QasXspress3Detector(XspressTrigger, Xspress3Detector):
         return ret
 
     def stage(self):
+        # self.external_trig.put(True)
+        # self.settings.trigger_mode.put(3)  # 'TTL Veto Only'
         if self.spectra_per_point.get() != 1:
             raise NotImplementedError(
                 "multi spectra per point not supported yet")
         ret = super().stage()
         # self._resource_uid = self._resource
         return ret
+
+    # def unstage(self):
+    #     self.settings.trigger_mode.put(0)  # 'Software'
+    #     super().unstage()
 
     # Currently only using three channels. Uncomment these to enable more
     # channels:
@@ -77,6 +83,36 @@ xs.channel2.vis_enabled.put(1)
 xs.channel3.vis_enabled.put(1)
 xs.settings.num_channels.put(4)
 
+
+class XSFlyer:
+    def __init__(self, det, pbs, pb_trigger, motor):
+        # self.name = f'{det.name}-{"-".join([pb.name for pb in pbs])}-flyer'
+        self.parent = None
+        self.det = det  # xspress3 device
+        self.pbs = pbs  # a list of passed pizza-boxes
+        self.motor = motor
+        self.pb_trigger = pb_trigger
+        self._motor_status = None
+
+    def kickoff(self, *args, **kwargs):
+        self.det.stage()
+        self.det.total_points.put()
+
+    def calc_num_points(self):
+        tr = trajectory_manager(self.motor)
+        info = tr.read_info(silent=True)
+        lut = str(int(self.motor.lut_number_rbv.get()))
+        traj_duration = int(info[lut]['size']) / 16000
+        if self.pb_trigger.unit_sel.get()==0:
+             multip = 1e-6
+        else:
+             multip = 1e-3
+        acq_num_points = traj_duration/ (self.pb_trigger.period_sp.get() * multip) * 1.3
+        print(f'acq_num_points: {acq_num_points}\ntraj_dur: {traj_duration}')
+        self.num_points = int(round(acq_num_points, ndigits=0))
+
+xsflyer = XSFlyer(xs, [], pb2.do1, mono1)
+
 # This is necessary for when the ioc restarts
 # we have to trigger one image for the hdf5 plugin to work correclty
 # else, we get file writing errors
@@ -87,15 +123,15 @@ for n in [1, 2]:
     getattr(xs, f'channel{n}').rois.roi01.value.kind = 'hinted'
 
 # import skbeam.core.constants.xrf as xrfC
-# 
+#
 # interestinglist = ['Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U']
-# 
+#
 # elements = dict()
 # element_edges = ['ka1','ka2','kb1','la1','la2','lb1','lb2','lg1','ma1']
 # element_transitions = ['k', 'l1', 'l2', 'l3', 'm1', 'm2', 'm3', 'm4', 'm5']
 # for i in interestinglist:
 #     elements[i] = xrfC.XrfElement(i)
-# 
+#
 # def setroi(roinum, element, edge):
 #     '''
 #     Set energy ROIs for Vortex SDD.  Selects elemental edge given current energy if not provided.
@@ -103,17 +139,17 @@ for n in [1, 2]:
 #     element     <symbol>    element symbol for target energy
 #     edge                    optional:  ['ka1','ka2','kb1','la1','la2','lb1','lb2','lg1','ma1']
 #     '''
-#     
+#
 #     cur_element = xrfC.XrfElement(element)
-# 
+#
 #     e_ch = int(cur_element.emission_line[edge] * 1000)
-#     
+#
 #     for (n, d) in xs.channels.items():
 #         d.set_roi(roinum, e_ch-200, e_ch+200, name=element + '_' + edge)
 #         getattr(d.rois, f'roi{roinum:02d}').kind = 'normal'
 #     print("ROI{} set for {}-{} edge.".format(roinum, element, edge))
-# 
-# 
+#
+#
 # def clearroi(roinum=None):
 #     if roinum is None:
 #         roinum = [1, 2, 3]
@@ -121,14 +157,14 @@ for n in [1, 2]:
 #         roinum = list(roinum)
 #     except TypeError:
 #         roinum = [roinum]
-# 
+#
 #     # xs.channel1.rois.roi01.clear
 #     for (n, d) in xs.channels.items():
 #         for roi in roinum:
 #              cpt = getattr(d.rois, f'roi{roi:02d}')
 #              cpt.clear()
 #              cpt.kind = 'omitted'
-# 
+#
 xs.settings.configuration_attrs = ['acquire_period',
 			           'acquire_time',
 			           'gain',
