da6aa1a35eb7bbbf66f8bbbd46daa29c77e812df
Maksim Rakitin <mrakitin@bnl.gov>
2022-09-30 11:26:34 -0400

    Add logging to file for num of open files, etc.

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -2,26 +2,45 @@ print(__file__)
 
 # Try to capture 'core dump' reasons.
 import faulthandler
-from pprint import pprint
+from pprint import pprint, pformat
 faulthandler.enable()
 
+import appdirs
 import sys
 import psutil
 import subprocess
+from pathlib import Path
+
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+logger_open_files = logging.getLogger("QAS")
+logger_open_files.setLevel(logging.DEBUG)
+# logger_open_files.setLevel(logging.INFO)
+debug_log_file = str(Path(appdirs.user_log_dir(appname="bluesky")) / Path("debug-open-files.log"))
+handler1 = TimedRotatingFileHandler(debug_log_file, when="W0", backupCount=10)
+log_file_format = (
+    "[%(levelname)1.1s %(asctime)s.%(msecs)03d %(name)s"
+    "  %(module)s:%(lineno)d] %(message)s"
+)
+handler1.setFormatter(logging.Formatter(fmt=log_file_format))
+logger_open_files.addHandler(handler1)
+
 
 def audit(event, args):
     if event == "open":
-        print(f"Opening file: {args}")
+        logger_open_files.debug(f"Opening file: {args}")
+
 
 def stop_callback(name, doc):
-    if name == 'stop':
-        res = subprocess.run("cat /proc/sys/fs/file-nr".split(), capture_output=True)
-        nums = res.stdout.decode().split()
+    res = subprocess.run("cat /proc/sys/fs/file-nr".split(), capture_output=True)
+    nums = res.stdout.decode().split()
 
-        proc = psutil.Process()
+    proc = psutil.Process()
 
-        print(f"\nCurrent open files: {nums[0]}  |  Max open files: {nums[-1]}\n")
-        pprint(proc.open_files())
+    logger_open_files.info(f"\nBluesky scan UID: {doc['run_start']}\n"
+                           f"Current open files: {nums[0]}  |  Max open files: {nums[-1]}\n"
+                           f"{pformat(proc.open_files())}")
 
 
 import nslsii
@@ -56,7 +75,7 @@ nslsii.configure_kafka_publisher(RE, 'qas')
 
 # TODO: remove after testing.
 sys.addaudithook(audit)
-RE.subscribe(stop_callback)
+RE.subscribe(stop_callback, name='stop')
 
 # nslsii.configure_base(get_ipython().user_ns, beamline_id, bec=False)
 
@@ -121,10 +140,6 @@ USER_FILEPATH = 'processed'
 
 #def print_to_gui(string, stdout=sys.stdout):
 #    print(string, file=stdout, flush=True)
-from pathlib import Path
-
-import appdirs
-
 
 try:
     from bluesky.utils import PersistentDict
